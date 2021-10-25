b39e8cdfb3532043a264d2f966d36b99b8a90a37
Khoby790 <ebaidoo79@gmail.com>
2021-10-25 07:58:01 +0000

    Server bootstrapped succesfully

---
+++ index.js
@@ -0,0 +1,62 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// Schemas
+// Resolvers
+
+//typeDefs
+
+const books = [
+    {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J.K. Rowling",
+        ISBN: "0-7475-3269-9"
+    },
+    {
+        title: "Jurassic Park",
+        author: "Michael Crichton",
+        ISBN: "0-7475-3269-9"
+    }
+];
+
+
+const schemas = gql`
+    type Book {
+        title: String!
+        author: String!
+        ISBN: String
+    }
+
+    type Query {
+        books: [Book]
+        book(title: String!): Book
+    }
+
+    type Mutation {
+        createBook(title: String!, author: String!, ISBN: String): Book
+    }
+
+    # type Subscription {}
+`;
+
+const booksResolvers = {
+    Query: {
+        books: () => books,
+        book: (parent, args) => books.find(book => book.title === args.title)
+    },
+    Mutation: {
+        createBook: (parent, args) => {
+            const { title, author, ISBN } = args;
+            const book = { title, author, ISBN };
+            books.push(book);
+            return book;
+        }
+    },
+}
+
+
+const server = new ApolloServer({ typeDefs: schemas, resolvers: booksResolvers });
+
+server.listen(8000).then(({ url }) => {
+    console.log(`Server ready at ${url} and ready to be used`);
+}).catch(err => console.log(err.message));
+
